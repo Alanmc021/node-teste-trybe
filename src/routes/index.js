bbd76b44f57049fce87a4dfbdc180d3f9fc05c24
dev-alan <alanmc021@gmail.com>
2022-11-30 16:32:09 -0300

    instal and config mongodb

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,14 +2,14 @@ import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (_req, res) => {
-    res.status(200).json({ ok: 'okkk' });
-});
+// routes.get('/', (_req, res) => {
+//     res.status(200).json({ ok: 'okkk' });
+// });
 
 // Não remover esse end-point, ele é necessário para o avaliador
-// routes.get('/', (request, response) => {
-//   response.send('end point zica');
-// });
+routes.get('/', (request, response) => {
+  response.send('end point zica');
+});
 // Não remover esse end-point, ele é necessário para o avaliador
 
 export default routes;
